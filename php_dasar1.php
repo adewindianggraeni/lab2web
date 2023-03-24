2b4143f2a694fafe3260064589f9f49821947148
anggraeniwindii1053@gmail.com <anggraeniwindi10@mhs.pelitabangsa.ac.id>
2023-03-24 15:24:03 +0700

    commit

---
+++ php_dasar1.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP Dasar</title>
+</head>
+
+<body>
+    <h1>Belajar PHP Dasar</h1>
+    <?php
+    echo "Hello World";
+
+    $nim = "312110227"
+    $nama = "Ade Windi Anggraeni"
+    echo "Nim: " . $nim . "<br>";
+    echo "Nama: " . $nama;
+    ?>
+</body>
+
+</html>
+Footer
+© 2023 GitHub, Inc.
+Footer
